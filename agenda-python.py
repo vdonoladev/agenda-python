395382a40b176deb2c1ff49bbbc99a9de01188e6
vdonoladev <victordonolaf@gmail.com>
2020-07-08 15:44:19 -0300

    Pequenas alterações

--- agenda-python.py
+++ agenda-python.py
@@ -8,9 +8,9 @@
 
 
 #--------------------GLOBAL--------------------#
-lista = [] #define uma variavel para a lista
+lista = [] # define uma variável para a lista
 
-#le os dados do arquivo e joga pra lista
+# lê os dados do arquivo e joga para a lista
 
 arq2 = open("agenda.csv","a+")
 lista = arq2.readlines()
@@ -20,7 +20,7 @@ arq2.close()
 
 #------------------------------------------------------------------------------#
 
-#função: menu, constroi o menu
+# função: menu, constroi o menu
 
 def menu():
     print ("\n", "=" * 50) 
@@ -31,27 +31,27 @@ def menu():
              (3) Mostrar agenda:\n
              (4) Finalizar programa:\n""")
     
-#fim da função menu
+# fim da função menu
 
 
 
 #------------------------------------------------------------------------------#
     
-#função: lista_agenda, adiciona, remove e mostra a lista
+# função: lista_agenda, adiciona, remove e mostra a lista
     
 def lista_agenda(nome, data, opc):
 
     if( opc == 1):
-        contato = nome + ";" + data + "\n" #concatenao o 'nome' ';' e 'data'
+        contato = nome + ";" + data + "\n" # concatena o 'nome' ';' e 'data'
         lista.append(contato)
-        lista.sort() #ordena a lista por prioridade
+        lista.sort() # ordena a lista por prioridade
 
     elif (opc == 2):
         print ("=" * 30)
-        if ( lista == []): #se lista for vazia
+        if ( lista == []): # se lista for vazia
             print ("Lista Vazia\n")
         else:
-            lista.pop(0)#remove o elemento de maior prioridade, ou seja, indice 0 da lista
+            lista.pop(0)# remove o elemento de maior prioridade, ou seja, índice 0 da lista
             print ("Removido o elemento de maior prioridade")
         print ("=" * 30)
 
@@ -66,32 +66,32 @@ def lista_agenda(nome, data, opc):
         print ("=" * 30)
 
     elif (opc == 4):
-        arq = open("agenda.csv","w") #escreve por cima do arquivo antigo (atualiza lista)
-        tam = len(lista) #recebe o tamanho da lista
-        #for que insere os valores no arquivo separado por ';' sendo nome = indice par e data = impar
+        arq = open("agenda.csv","w") # escreve por cima do arquivo antigo (atualiza lista)
+        tam = len(lista) # recebe o tamanho da lista
+        # for que insere os valores no arquivo separado por ';' sendo nome = índice par e data = impar
 
         for i in range(tam):
             arq.write(lista[i])
 
         arq.close()
 
-#fim da função lista_agenda
+# fim da função lista_agenda
 
 
         
 #------------------------------------------------------------------------------#
-#funcao principal
+# função principal
 
 
-opc = 0 #variavel pro menu
+opc = 0 # variável para o menu
 
 while (opc != 4 ):
 
-    menu() #chama o menu
+    menu() # chama o menu
 
     while True:
         try:
-            opc = int(input('')) #recebe a opção
+            opc = int(input('')) # recebe a opção
             break
         except:
             print ("Digite somente numeros válidos")
@@ -105,19 +105,19 @@ while (opc != 4 ):
         #print ("informe a data de nascimento: \n")
         # data = input('\n: ')
 
-        lista_agenda(nome, data, 1) #chama a função para inserir na lista
+        lista_agenda(nome, data, 1) # chama a função para inserir na lista
 
     elif ( opc == 2):
-        lista_agenda(0,0,2) #chama a função para deletar o nome na lista
+        lista_agenda(0,0,2) # chama a função para deletar o nome na lista
 
-    elif (opc == 3): #chama a função para mostrar na tela
+    elif (opc == 3): # chama a função para mostrar na tela
         print ("Lista de contatos: ")
         lista_agenda(0,0,3)
                 
 
-lista_agenda(0,0,4) #grava a lista na agenda
+lista_agenda(0,0,4) # grava a lista na agenda
 print ("FIM DO PROGRAMA! ")
 
 #------------------------------------------------------------------------------#
-#fim
+# fim
     
